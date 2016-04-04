296d8ae8f66b4be1e2b7ff0881156b4dd88aef44
Prokhor Mikhaylov <prohormihailov@gmail.com>
2016-04-04 13:19:02 +0500

    Change default font

--- config/visual/init-font.el
+++ config/visual/init-font.el
@@ -1,5 +1,5 @@
 
-(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-13"))
-(set-face-attribute 'mode-line nil :family "Ubuntu Mono")
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-13:medium"))
+(set-face-attribute 'mode-line nil :family "Source Code Pro")
 
 (provide 'init-font)
