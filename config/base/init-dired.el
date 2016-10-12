d14e0d62f0595fc904e20bd19b9c314b21012041
Prokhor Mikhaylov <prohormihailov@gmail.com>
2016-10-12 13:39:06 +0500

    [new] Added ordering to dired mode

--- config/base/init-dired.el
+++ config/base/init-dired.el
@@ -13,4 +13,17 @@
 ;; Auto-refresh dired on file change
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header 
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+  (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
 (provide 'init-dired)
